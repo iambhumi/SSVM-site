26237f34e72ab3972f40b9ee16abead03b028f4a
Bhumi Sarkar <bhumi.sarkar129@gmail.com>
2026-04-18 14:23:48 +0530

    minor changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-
+import ScrollToTop from "../components/ScrollToTop";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
@@ -6,6 +6,7 @@ import Footer from "./Footer";
 export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-grow">
         <Outlet />
